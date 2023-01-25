53eb5e09240c1f3d499077dbaf9603de0bf998bf
JongKook <94429120+JongKookE@users.noreply.github.com>
2023-01-25 22:11:06 +0900

    Add files via upload

--- src/main/java/com/example/OAuth/OAuthApplication.java
+++ src/main/java/com/example/OAuth/OAuthApplication.java
@@ -1,11 +1,17 @@
 package com.example.OAuth;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.OAuth.accessingdatajpa.Customer;
@@ -16,6 +22,11 @@ import com.example.OAuth.accessingdatajpa.CustomerRepository;
 public class OAuthApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(OAuthApplication.class);
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal){
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OAuthApplication.class, args);
